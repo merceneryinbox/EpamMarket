735232202b22786e690d4f8b43da21570671a6e0
mercenery <mercenery@inbox.ru>
2017-12-13 20:22:58 +0300

    db fulfill ready dump ready

---
+++ src/resources/postgres_public_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (login, email, phone, password, status, id) VALUES ('admin_login', 'admin@mail.ru', '+79219896890', '123', 'administrator', 1);
+INSERT INTO public.users (login, email, phone, password, status, id) VALUES ('active_user_login', 'active@mail.ru', '+7777777777', '1', 'active', 2);
+INSERT INTO public.users (login, email, phone, password, status, id) VALUES ('blocked_user_login', 'blocked@mail.ru', '+7888888888', '2', 'blocked', 3);
