687df645024e1670a9bafbf72422f85fe5cc5a24
mercenery <mercenery@inbox.ru>
2017-12-13 19:46:27 +0300

    db dump ready

---
+++ src/resources/CREATE TABLE cart users goods.sql
@@ -0,0 +1,32 @@
+CREATE TABLE cart
+(
+    id INTEGER DEFAULT nextval('cart_id_seq'::regclass) PRIMARY KEY NOT NULL,
+    amount_cart INTEGER DEFAULT 1 NOT NULL,
+    reserve_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    id_good INTEGER NOT NULL,
+    CONSTRAINT cart_fk_id_good FOREIGN KEY (id_good) REFERENCES goods (id)
+);
+CREATE UNIQUE INDEX cart_id_uindex ON cart (id);
+CREATE TABLE goods
+(
+    id INTEGER DEFAULT nextval('goods_id_seq'::regclass) PRIMARY KEY NOT NULL,
+    product_name VARCHAR(256) NOT NULL,
+    price REAL NOT NULL,
+    amount INTEGER NOT NULL,
+    description VARCHAR(1024) NOT NULL,
+    image VARCHAR(256)
+);
+CREATE UNIQUE INDEX goods_id_uindex ON goods (id);
+CREATE TABLE users
+(
+    login VARCHAR(32) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    phone VARCHAR(32) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    status VARCHAR(32) DEFAULT false NOT NULL,
+    id INTEGER DEFAULT nextval('users_id_user_seq'::regclass) PRIMARY KEY NOT NULL
+);
+CREATE UNIQUE INDEX users_login_uindex ON users (login);
+CREATE UNIQUE INDEX users_email_uindex ON users (email);
+CREATE UNIQUE INDEX users_phone_number_uindex ON users (phone);
+CREATE UNIQUE INDEX users_id_user_uindex ON users (id);
