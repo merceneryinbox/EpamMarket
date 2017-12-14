c4874238a4071db57ffb124669143aa7a1e8fa93
Vladimir Shefer <quameu@gmail.com>
2017-12-14 19:07:42 +0300

    Camel case applied in Reserve entities

--- src/main/java/entities/Reserve.java
+++ src/main/java/entities/Reserve.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class Reserve {
     private Integer id;
     private String login;
-    private Integer good_id;
+    private Integer goodId;
     private Integer amount;
-    private String reserve_time;
+    private String reserveTime;
 }
