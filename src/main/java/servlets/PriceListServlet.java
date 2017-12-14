773509805c337b682ccbe5a2a7975b85f85ae230
mercenery <mercenery@inbox.ru>
2017-12-14 19:43:49 +0300

    servlets rename

--- src/main/java/servlets/PriceListServlet.java
+++ src/main/java/servlets/PriceListServlet.java
@@ -1,12 +1,14 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UnsignedUserPage extends HttpServlet {
+@WebServlet("/price_list")
+public class PriceListServlet extends HttpServlet {
 	@Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		super.doGet(req, resp);
