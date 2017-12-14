c0954a0cec4f4df31aac8e723b32caa7fbae201b
Vladimir Shefer <quameu@gmail.com>
2017-12-14 21:18:17 +0300

    +ReserveDAO interface draft

--- src/main/java/dao/ReserveDao.java
+++ src/main/java/dao/ReserveDao.java
@@ -1,4 +1,14 @@
 package dao;
 
-public class ReserveDao {
+import entities.Reserve;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReserveDao {
+    Optional<List<Reserve>> getReserveListByLogin(String login);
+    Optional<List<Reserve>> getReserveListByGoodId(Integer goodId);
+    Optional<List<Reserve>> getReserveByLoginAndGoodId(String login, Integer GoodId);
+
+    void addReserve(Reserve reserve);
 }
