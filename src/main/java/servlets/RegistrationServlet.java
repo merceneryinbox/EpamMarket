cd6f0eb25e8abb155d6acb4a9fa3f42aae87991a
mercenery <mercenery@inbox.ru>
2017-12-15 13:37:21 +0300

    CartServlet add session + sessions attributes

--- src/main/java/servlets/RegistrationServlet.java
+++ src/main/java/servlets/RegistrationServlet.java
@@ -5,13 +5,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/sign_up")
 public class RegistrationServlet extends HttpServlet {
+	String userName = null;
+	String userPass = null;
+	
 	@Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
 		req.getRequestDispatcher("sign_up.jsp")
 				.forward(req, resp);
+		if (req != null) {
+			
+			userName = (String) req.getAttribute("name");
+			userPass = (String) req.getAttribute("password");
+			HttpSession cartSession = req.getSession();
+			if (cartSession == null) {
+				cartSession.setAttribute("name", userName);
+				cartSession.setAttribute("password", userPass);
+			}
+		}
 	}
 }
