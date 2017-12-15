668538b18baaf88c0868e76248f54a500d3ed37c
mercenery <mercenery@inbox.ru>
2017-12-15 18:26:09 +0300

    7.5 complete

--- src/main/java/servlets/CartServlet.java
+++ src/main/java/servlets/CartServlet.java
@@ -10,16 +10,19 @@ import java.io.IOException;
 
 @WebServlet("/cart")
 public class CartServlet extends HttpServlet {
-	String login    = null;
-	String password = null;
-	long   cartId   = 0;
+	
 	
 	@Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String login;
+		String password;
+		long   cartId;
+		
 		if (req != null) {
 			login = (String) req.getAttribute("name");
 			password = (String) req.getAttribute("password");
 			cartId = (Long) req.getAttribute("id_cart");
+			
 			HttpSession cartSession = req.getSession();
 			if (login != null && password != null && cartId != 0) {
 				cartSession.setAttribute("name", login);
@@ -32,18 +35,6 @@ public class CartServlet extends HttpServlet {
 	}
 	
 	@Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		req.getRequestDispatcher("cart.jsp")
-				.forward(req, resp);
-	}
-	
-	@Override protected void doPut(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		req.getRequestDispatcher("cart.jsp")
-				.forward(req, resp);
-	}
-	
-	@Override protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		req.getRequestDispatcher("cart.jsp")
 				.forward(req, resp);
