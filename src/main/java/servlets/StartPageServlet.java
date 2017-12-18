e59e40c5387c0a5550cef5f7f024a23b4afb307e
mercenery <mercenery@inbox.ru>
2017-12-18 23:38:03 +0300

    Create Start page servlet

--- src/main/java/servlets/StartPageServlet.java
+++ src/main/java/servlets/StartPageServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "StartPage", value = "/")
@@ -19,17 +18,6 @@ public class StartPageServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException {
-		
-		HttpSession session = req.getSession();
-		
-		if (session != null) {
-			String login = (String) session.getAttribute("login");
-			String password = (String) session.getAttribute("password");
-			if (login != null && password != null) {
-				req.getRequestDispatcher("login.jsp").forward(req, resp);
-			} else {
-				req.getRequestDispatcher("index.jsp").forward(req, resp);
-			}
-		}
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
 }
