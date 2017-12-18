aa4938fa682e80871509d41f524272f974773f83
Igor <igorer27@yandex.ru>
2017-12-18 21:51:30 +0300

    Change query for cart

--- src/main/resources/cart.sql
+++ src/main/resources/cart.sql
@@ -1,14 +1,12 @@
 CREATE TABLE cart
 (
   cart_id SERIAL PRIMARY KEY NOT NULL,
-  login VARCHAR(256) NOT NULL,
-  good_name VARCHAR(256) NOT NULL,
+  user_id INTEGER NOT NULL,
+  goods_id INTEGER NOT NULL,
   amount INTEGER DEFAULT 1 NOT NULL,
   reserve_time TIMESTAMP DEFAULT current_timestamp NOT NULL,
-  CONSTRAINT cart___fk_user_login FOREIGN KEY (login) REFERENCES users (login),
-  CONSTRAINT cart___fk_goods_name FOREIGN KEY (good_name) REFERENCES goods (name)
+  CONSTRAINT cart___fk__users FOREIGN KEY (user_id) REFERENCES users (user_id),
+  CONSTRAINT cart___fk__goods FOREIGN KEY (goods_id) REFERENCES goods (goods_id)
 );
 CREATE UNIQUE INDEX cart_cart_id_uindex ON cart (cart_id);
-CREATE UNIQUE INDEX cart_login_uindex ON cart (login);
-CREATE UNIQUE INDEX cart_good_name_uindex ON cart (good_name);
 
