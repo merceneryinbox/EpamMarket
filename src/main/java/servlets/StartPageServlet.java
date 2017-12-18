c17d1c4523c4b2f92fdb1c6e1593d9994bf220ff
mercenery <mercenery@inbox.ru>
2017-12-18 23:19:59 +0300

    6.1_Create_start_page_servlet

--- src/main/java/servlets/StartPageServlet.java
+++ src/main/java/servlets/StartPageServlet.java
@@ -5,18 +5,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "StartPage", value = "/")
 public class StartPageServlet extends HttpServlet {
-	@Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+			IOException {
 		doGet(req, resp);
 	}
 	
-	@Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		req.getRequestDispatcher("/index.jsp")
-				.forward(req, resp);
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+			IOException {
+		
+		HttpSession session = req.getSession();
+		
+		if (session != null) {
+			String login = (String) session.getAttribute("login");
+			String password = (String) session.getAttribute("password");
+			if (login != null && password != null) {
+				req.getRequestDispatcher("login.jsp").forward(req, resp);
+			} else {
+				req.getRequestDispatcher("index.jsp").forward(req, resp);
+			}
+		}
 	}
 }
