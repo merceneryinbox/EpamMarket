0afcfbc0fd6f023921581cd6cc259f33d755d6e7
Igor <igorer27@yandex.ru>
2017-12-18 20:55:16 +0300

    Fix bug for sql-query of cart create

--- src/main/resources/cart.sql
+++ src/main/resources/cart.sql
@@ -5,7 +5,7 @@ CREATE TABLE cart
   good_name VARCHAR(256) NOT NULL,
   amount INTEGER DEFAULT 1 NOT NULL,
   reserve_time TIMESTAMP DEFAULT current_timestamp NOT NULL,
-  CONSTRAINT cart___fk_user_login FOREIGN KEY (login) REFERENCES public.users (login),
+  CONSTRAINT cart___fk_user_login FOREIGN KEY (login) REFERENCES users (login),
   CONSTRAINT cart___fk_goods_name FOREIGN KEY (good_name) REFERENCES goods (name)
 );
 CREATE UNIQUE INDEX cart_cart_id_uindex ON cart (cart_id);
