5f3bef281ef57af8ab67620a598672e8cb37f117
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-12-19 18:42:38 +0300

    Add service usage in servlet

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
+import entities.User;
 import services.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,63 +15,56 @@ import java.io.IOException;
 @WebServlet(name = "SignIn", value = "/sign_in")
 public class SignInServlet extends HttpServlet {
 
-	private String checker;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		HttpSession session = req.getSession();
-		String login;
-		String password;
-		login = (String) session.getAttribute("login");
-		password = (String) session.getAttribute("password");
-		if (login != null && password != null) {
-			if (login.equalsIgnoreCase("admin")) {
-				req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
-			} else {
-				req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
-			}
-		} else {
-			req.getRequestDispatcher("/signin.jsp").forward(req, resp);
-		}
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String login;
+        String password;
+        login = (String) session.getAttribute("login");
+        password = (String) session.getAttribute("password");
+        if (login != null && password != null) {
+            if (login.equalsIgnoreCase("admin")) {
+                req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
+            } else {
+                req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
+            }
+        } else {
+            req.getRequestDispatcher("/signin.jsp").forward(req, resp);
+        }
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		HttpSession session = req.getSession();
-		String login;
-		String password;
-		login = (String) req.getParameter("login");
-		password = (String) req.getParameter("password");
-		//TODO: logic of verification login and password in DB
-		//TODO: logic of checking login's status
-		checker = "active";
-		if (login.equals("admin")) {
-			checker = "admin";
-		}
-		if (login.equals("banned")) {
-			checker = "banned";
-		}
-		if (checker.equalsIgnoreCase("banned")) {
-			req.getRequestDispatcher("/banneduser.jsp")
-					.forward(req, resp);
-		} else if (checker.equalsIgnoreCase("active")) {
-			session.setAttribute("login", login);
-			session.setAttribute("password", password);
-			req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
-		} else if (checker.equalsIgnoreCase("admin")) {
-			session.setAttribute("login", login);
-			session.setAttribute("password", password);
-			req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
-		}
-	}
-
-
-	private String checkUser(String login, String password) {
-		String result = "active";
-		UserService userService = new UserService(login, password);
-		result = userService.checkBanInBase();
-		return result;
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String login;
+        String password;
+        String status;
+        login = req.getParameter("login");
+        password = req.getParameter("password");
+        User user;
+        user = UserCheckPasswordService.checkPassword(login, password);
+        if (user == null) {
+            req.getRequestDispatcher("/signin.jsp").forward(req, resp);
+        } else {
+            status = user.getStatus();
+            if (status.equalsIgnoreCase("banned")) {
+                req.getRequestDispatcher("/banneduser.jsp")
+                        .forward(req, resp);
+            } else if (status.equalsIgnoreCase("active")) {
+                session.setAttribute("login", login);
+                session.setAttribute("password", password);
+                session.setAttribute("status", status);
+                req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
+            } else if (status.equalsIgnoreCase("admin")) {
+                session.setAttribute("login", login);
+                session.setAttribute("password", password);
+                session.setAttribute("status", status);
+                req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
+            }
+        }
+    }
+    
 }
