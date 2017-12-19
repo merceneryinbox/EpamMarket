31554bc607f1cbfd3602507387f95651faea7182
mercenery <mercenery@inbox.ru>
2017-12-19 19:09:04 +0300

    6.7 - Rebuild PriceList Servlet + add service AllGoodGetterService

---
+++ src/main/java/services/AllGoodsGetterService.java
@@ -0,0 +1,13 @@
+package services;
+
+import entities.Good;
+
+import java.util.List;
+
+public class AllGoodsGetterService {
+	private List<Good> priceList;
+	
+	public List<Good> getPriceList() {
+		return priceList;
+	}
+}
--- src/main/java/servlets/PriceListServlet.java
+++ src/main/java/servlets/PriceListServlet.java
@@ -1,11 +1,18 @@
 package servlets;
 
+import entities.Good;
+import services.AllGoodsGetterService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "PriceList", value = "/price_list")
 public class PriceListServlet extends HttpServlet {
@@ -18,6 +25,12 @@ public class PriceListServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException {
-		req.getRequestDispatcher("/pricelist.jsp");
+		AllGoodsGetterService allGoodsGetter = new AllGoodsGetterService();
+		Map<String, List<Good>> priceList = new HashMap<>();
+		priceList.put("priceList", allGoodsGetter.getPriceList());
+		HttpSession session = req.getSession();
+		session.setAttribute("priceList", priceList);
+		
+		req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
 	}
 }
