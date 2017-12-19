5acbcdca3b98d773593592a84a5dedeaeb54c99f
mercenery <mercenery@inbox.ru>
2017-12-19 20:19:25 +0300

    6.7 - Rebuild PriceList Servlet + add service AllGoodGetterService

--- src/main/java/servlets/SignUpServlet.java
+++ src/main/java/servlets/SignUpServlet.java
@@ -4,15 +4,16 @@ import dao.PostgresUserDAO;
 import entities.User;
 import services.UserStatus;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(name = "Registrarion", value = "/sign_up")
 public class SignUpServlet extends HttpServlet {
-	
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
@@ -31,20 +32,30 @@ public class SignUpServlet extends HttpServlet {
 			phone = (String) req.getAttribute("phone");
 			statusDefault = UserStatus.ACTIVE.name();
 			
-			user.setLogin(login);
-			user.setPassword(password);
-			user.setEmail(email);
-			user.setPhone(phone);
-			user.setStatus(statusDefault);
-			
-			try {
-				HttpSession registrationSession = req.getSession();
-				registrationSession.setAttribute("user", user);
-				postgresUserDAO.createNew(user);
+			if (login != null && password != null) {
+				user.setLogin(login);
+				user.setPassword(password);
+				user.setEmail(email);
+				user.setPhone(phone);
+				user.setStatus(statusDefault);
 				
-				req.getRequestDispatcher("pricelist.jsp").forward(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
+				try {
+					HttpSession registrationSession = req.getSession();
+					registrationSession.setAttribute("user", user);
+					postgresUserDAO.createNew(user);
+					
+					req.getRequestDispatcher("/pricelist.jsp").forward(req, resp);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					req.getRequestDispatcher("/signup.jsp").forward(req, resp);
+				} catch (ServletException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
