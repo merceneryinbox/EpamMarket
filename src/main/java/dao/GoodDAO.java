40ad65218a0770eb7c5e810a0df5570c60e402f6
Igor <igorer27@yandex.ru>
2017-12-20 18:54:29 +0300

    Add template for method getAllGoods

--- src/main/java/dao/GoodDAO.java
+++ src/main/java/dao/GoodDAO.java
@@ -1,10 +1,13 @@
 package dao;
 
 import entities.Good;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface GoodDAO {
     Optional<Good> getGoodByName(String name);
+    List<Good> getAllGoods();
     void addGood(Good good);
     void deleteGoodByName(String name);
     void updateGood(Good good);
