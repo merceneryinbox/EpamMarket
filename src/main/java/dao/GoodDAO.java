80695e0eda47095a15fb90504196ffda2ee5522b
Vladimir Shefer <quameu@gmail.com>
2017-12-20 17:25:07 +0300

    Added getById methods into CartDao and GoodDao

--- src/main/java/dao/GoodDAO.java
+++ src/main/java/dao/GoodDAO.java
@@ -1,11 +1,17 @@
 package dao;
 
 import entities.Good;
+
 import java.util.Optional;
 
 public interface GoodDAO {
+    Optional<Good> getGoodById(Integer id);
+
     Optional<Good> getGoodByName(String name);
+
     void addGood(Good good);
+
     void deleteGoodByName(String name);
+
     void updateGood(Good good);
 }
