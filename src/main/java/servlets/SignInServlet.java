274d5c9763003d1f467de040ac57387654065da7
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-12-21 16:51:10 +0300

    add switch and enum to get-method of servlet

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -21,15 +21,20 @@ public class SignInServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         HttpSession session = req.getSession();
-        String status;
+        UserStatus status;
         User user;
         user = (User) session.getAttribute("user");
         if (user != null) {
-            status = user.getStatus();
-            if (status.equalsIgnoreCase("admin")) {
-                req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
-            } else {
-                resp.sendRedirect("/price_list");
+            status = UserStatus.valueOf(user.getStatus());
+            switch (status) {
+                case ACTIVE:
+                    session.setAttribute("user", user);
+                    req.getRequestDispatcher("/price_list").forward(req,resp);
+                    break;
+                case ADMIN:
+                    session.setAttribute("user",user);
+                    req.getRequestDispatcher("/adminpage.jsp").forward(req,resp);
+                    break;
             }
         } else {
             req.getRequestDispatcher("/signin.jsp").forward(req, resp);
