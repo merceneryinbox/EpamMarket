8d0c4fd52759bfa756d07208acd7272c31c6bb8c
Vladimir Shefer <quameu@gmail.com>
2017-12-21 20:31:13 +0300

    AdminServlet created

---
+++ src/main/java/servlets/AdminServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import entities.User;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Admin", value = "/admin")
+public class AdminServlet extends HttpServlet{
+
+    class AdminService {
+        public List<User> getUserList(){
+            throw new NotImplementedException();
+        }
+    }
+    AdminService adminService = new AdminService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> userList = adminService.getUserList();
+        try {
+            req.setAttribute("users", userList);
+            req.getRequestDispatcher("/adminpage.jsp").forward(req, resp);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+}
