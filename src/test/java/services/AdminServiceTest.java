d3b75ae9755fb79825b4612af32182fb116acc85
Igor <igorer27@yandex.ru>
2017-12-25 21:33:44 +0300

    Fixes test

--- src/test/java/services/AdminServiceTest.java
+++ src/test/java/services/AdminServiceTest.java
@@ -3,6 +3,7 @@ package services;
 import dao.PostgresUserDAO;
 import db.DataSourceInit;
 import db.DatabaseManager;
+import entities.EntityGenerator;
 import entities.User;
 import lombok.val;
 import org.junit.jupiter.api.AfterEach;
@@ -25,16 +26,16 @@ class AdminServiceTest {
     @Test
     void getUserList() {
         val userDAO = PostgresUserDAO.getTestInstance();
-        val user = User.testUserForName("Vasya");
-        val user2 = User.testUserForName("Pete");
-        userDAO.createNew(user);
-        userDAO.createNew(user2);
+        val user = EntityGenerator.testUserForName("Vasya");
+        val user2 = EntityGenerator.testUserForName("Pete");
+        userDAO.addUser(user);
+        userDAO.addUser(user2);
         AdminService adminService = AdminService.getTestInstance();
         val list = adminService.getUserList();
 
         assertTrue(list.contains(user));
         assertTrue(list.contains(user2));
-        assertFalse(list.contains(User.testUserForName("Lilya")));
+        assertFalse(list.contains(EntityGenerator.testUserForName("Lilya")));
 
     }
 
