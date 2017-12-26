a1dcd2f3352b03a9927cdade0d6092ac32313f56
Andrei <Andrei_Bogus@epam.com>
2017-12-26 17:04:18 +0300

    unused import was deleted

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.xml.ws.http.HTTPException;
 import java.io.IOException;
 
 @Log4j2
